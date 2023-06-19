eb1d7c44c02df93ae940fdcc1152c721322cfebc
Timofei Belov <timbe2017@yandex.ru>
2023-06-19 10:51:28 +0300

    remake link on api

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const title = document.querySelector(".js-title");
 const SERVER_SUCCESS_RESPONSE_CLASS = "body-response";
 
 btn.addEventListener("click", function () {
-  fetch("http://www.boredapi.com/api/activity/")
+  fetch("https://www.boredapi.com/api/activity/")
     .then((response) => response.json())
     .then((data) => {
       outputText.innerHTML = data.activity;
