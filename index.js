b40c38e040fb164c07e0020cef823d4966a79253
Timofei Belov <timbe2017@yandex.ru>
2023-06-19 10:45:34 +0300

    do a bored app

---
+++ index.js
@@ -0,0 +1,16 @@
+const body = document.querySelector(".body");
+const btn = document.querySelector(".js-btn");
+const outputText = document.querySelector(".js-output");
+const title = document.querySelector(".js-title");
+
+const SERVER_SUCCESS_RESPONSE_CLASS = "body-response";
+
+btn.addEventListener("click", function () {
+  fetch("http://www.boredapi.com/api/activity/")
+    .then((response) => response.json())
+    .then((data) => {
+      outputText.innerHTML = data.activity;
+      title.innerHTML = "Ура, теперь не скучно 🔥";
+      body.classList.add(SERVER_SUCCESS_RESPONSE_CLASS);
+    });
+});
